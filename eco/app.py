b1f1b17e3733a409dfd742e82bbbae5af8682279
euang63 <eunag39@naver.com>
2021-07-21 11:15:45 +0900

    app.py 뼈대

---
+++ eco/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+## GET
+@app.route('/memo', methods=['GET'])
+def listing():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg':'GET 연결되었습니다!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
