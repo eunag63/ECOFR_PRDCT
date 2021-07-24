937bd44e4d04d20bb0eccec59596a7c522e27dff
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-07-24 18:55:43 +0900

    카테고리(kitchen,living,office) 기능 추가

--- eco/app.py
+++ eco/app.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbproject
 
 
 
@@ -15,12 +15,38 @@ db = client.dbsparta
 def home():
    return render_template('index.html')
 
-## GET
-@app.route('/eco', methods=['GET'])
+## 전체 보여주기
+@app.route('/re', methods=['GET'])
 def listing():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg':'GET 연결되었습니다!'})
+    product = list(db.crawling.find({},{'_id':False}))
+    return jsonify({'products':product})
+
+## 카테고리 living만 보여주기
+@app.route('/living')
+def getLiving():
+   return render_template('index_living.html')
+@app.route('/living/re', methods=['GET'])
+def cate_living():
+    product = list(db.crawling.find({'category':'living'}, {'_id': False}))
+    return jsonify({'products':product})
+
+## 카테고리 kitchen만 보여주기
+@app.route('/kitchen')
+def getKitchen():
+   return render_template('index_kitchen.html')
+@app.route('/kitchen/re', methods=['GET'])
+def cate_kitchen():
+    product = list(db.crawling.find({'category':'kitchen'}, {'_id': False}))
+    return jsonify({'products':product})
+
+## 카테고리 office만 보여주기
+@app.route('/office')
+def getOffice():
+   return render_template('index_office.html')
+@app.route('/office/re', methods=['GET'])
+def cate_office():
+    product = list(db.crawling.find({'category':'office'}, {'_id': False}))
+    return jsonify({'products':product})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
